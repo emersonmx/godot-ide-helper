2019b222485dcab861dfb84c7491ca6972bd9226
Emerson MX <emersonmx@gmail.com>
2017-09-08 01:14:58 -0300

    Create modules for objects

---
+++ objects.py
@@ -0,0 +1,17 @@
+class Constant(object):
+    pass
+
+class Signal(object):
+    pass
+
+class Member(object):
+    pass
+
+class Variable(object):
+    pass
+
+class Function(object):
+    pass
+
+class Class(object):
+    pass
