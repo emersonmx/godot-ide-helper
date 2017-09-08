4e51fc06c7151f52d3ee5847ed1918fc012751d0
Emerson MX <emersonmx@gmail.com>
2017-09-08 01:29:03 -0300

    Add base class

--- objects.py
+++ objects.py
@@ -1,17 +1,52 @@
-class Constant(object):
-    pass
+class Base(object):
+    def load_from_xml_element(self, element):
+        pass
 
-class Signal(object):
-    pass
+class Constant(Base):
+    def __init__(self):
+        super(Constant, self).__init__()
 
-class Member(object):
-    pass
+    def load_from_xml_element(self, element):
+        pass
 
-class Variable(object):
-    pass
+class Signal(Base):
+    def __init__(self):
+        super(Signal, self).__init__()
 
-class Function(object):
-    pass
+    def load_from_xml_element(self, element):
+        pass
+
+class Member(Base):
+    def __init__(self):
+        super(Member, self).__init__()
+
+    def load_from_xml_element(self, element):
+        pass
+
+class Variable(Base):
+    def __init__(self):
+        super(Variable, self).__init__()
+
+    def load_from_xml_element(self, element):
+        pass
+
+class Argument(Base):
+    def __init__(self):
+        super(Argument, self).__init__()
+
+    def load_from_xml_element(self, element):
+        pass
+
+class Function(Base):
+    def __init__(self):
+        super(Function, self).__init__()
+
+    def load_from_xml_element(self, element):
+        pass
 
 class Class(object):
-    pass
+    def __init__(self):
+        super(Class, self).__init__()
+
+    def load_from_xml_element(self, element):
+        pass
