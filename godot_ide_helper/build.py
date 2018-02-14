3b47aeef8cf94c95273dbae64898c7aa2bb6d320
Emerson MX <emersonmx@gmail.com>
2018-02-14 09:33:00 -0300

    Add zip package to current dir

--- godot_ide_helper/build.py
+++ godot_ide_helper/build.py
@@ -3,9 +3,10 @@
 
 import os
 import shutil
-import zipfile
 import click
 import requests
+
+from zipfile import ZipFile
 
 from godot_ide_helper.cli import cli
 from godot_ide_helper.utils import *
@@ -17,16 +18,16 @@ from godot_ide_helper.reader import *
 class Downloader:
 
     def __init__(self, version, cache=True):
-        self.dl_url_base = 'https://github.com/godotengine/godot/archive/'
-        self.version = version
-        self.cache = cache
+        self._dl_url_base = 'https://github.com/godotengine/godot/archive/'
+        self._version = version
+        self._cache = cache
 
     def get_download_url(self):
-        return os.path.join(self.dl_url_base, get_zip_filename(self.version))
+        return os.path.join(self._dl_url_base, get_zip_filename(self._version))
 
     def run(self):
-        output_path = get_zip_filepath(self.version)
-        if os.path.exists(output_path) and self.cache:
+        output_path = get_zip_filepath(self._version)
+        if os.path.exists(output_path) and self._cache:
             click.echo('Using cached version.')
             return
 
@@ -40,15 +41,15 @@ class Downloader:
 class Extractor:
 
     def __init__(self, version):
-        self.version = version
+        self._version = version
 
     def run(self):
-        zip_extract_path = get_zip_extraction_path(self.version)
+        zip_extract_path = get_zip_extraction_path(self._version)
         if os.path.exists(zip_extract_path):
             shutil.rmtree(zip_extract_path)
 
-        with zipfile.ZipFile(get_zip_filepath(self.version)) as zp:
-            doc_path = get_zip_doc_relpath(self.version)
+        with ZipFile(get_zip_filepath(self._version)) as zp:
+            doc_path = get_zip_doc_relpath(self._version)
             for file in zp.namelist():
                 if not file.startswith(doc_path):
                     continue
@@ -58,34 +59,46 @@ class Extractor:
 class Builder:
 
     def __init__(self, generator, version):
-        self.generator = generator
-        self.version = version
+        self._generator = generator
+        self._version = version
 
     def make_generator(self):
-        class_name = self.generator + 'Generator'
+        class_name = self._generator + 'Generator'
         return getattr(generators, class_name)
 
     def build_legacy(self):
         cls = self.make_generator()
-        inputfile = os.path.join(get_zip_doc_path(self.version), 'base/classes.xml')
-        output_path = get_scripts_path(self.version)
+        inputfile = os.path.join(get_zip_doc_path(self._version), 'base/classes.xml')
+        output_path = get_scripts_path(self._version)
         app = cls(inputfile, output_path)
         app.run()
 
     def build(self):
-        classes_path = os.path.join(get_zip_doc_path(self.version), 'classes')
+        classes_path = os.path.join(get_zip_doc_path(self._version), 'classes')
         for class_file in os.listdir(classes_path):
             cls = self.make_generator()
             inputfile = os.path.join(classes_path, class_file)
-            output_path = get_scripts_path(self.version)
+            output_path = get_scripts_path(self._version)
             app = cls(inputfile, output_path, False)
             app.run()
 
     def run(self):
-        if self.version < '3.0-stable':
+        if self._version < '3.0-stable':
             self.build_legacy()
         else:
             self.build()
+
+
+class Packer:
+
+    def __init__(self, version):
+        self._version = version
+
+    def run(self):
+        script_path = get_scripts_path(self._version)
+        with ZipFile('godot-{}-scripts.zip'.format(self._version), mode='w') as zp:
+            for file in os.listdir(script_path):
+                zp.write(os.path.join(script_path, file), file)
 
 @cli.command()
 @click.option('--generator', default='GDScript', type=click.Choice(['GDScript']),
@@ -115,5 +128,9 @@ def build(generator, cache, version):
     click.echo('Building stubs...')
     builder = Builder(generator, version)
     builder.run()
+
+    click.echo('Packing scripts...')
+    packer = Packer(version)
+    packer.run()
 
     click.echo('Done.')
