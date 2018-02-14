4a387c71bcbd024c3a0ca10918bda613cbd19b4d
Emerson MX <emersonmx@gmail.com>
2018-02-14 19:47:57 -0300

    Finish gdscript writer

--- godot_ide_helper/writer/gd_script_writer.py
+++ godot_ide_helper/writer/gd_script_writer.py
@@ -21,8 +21,11 @@ class GDScriptWriter(Writer):
     def _get_wrapped_lines(self, lines, columns=78):
         return textwrap.wrap(lines, columns)
 
+    def _indent_line(self, line):
+        return (self._indent_char * self._indent_level) + line
+
     def _make_line(self, line='', nl=True):
-        result = (self._indent_char * self._indent_level) + line
+        result = self._indent_line(line)
         result += '\n' if nl else ''
         return result
 
@@ -35,9 +38,12 @@ class GDScriptWriter(Writer):
         result = ''
         count_empty_lines = 0
         for line in [line.strip() for line in text.split('\n')]:
-            wlines = self._get_wrapped_lines(line)
-            result += '\n'.join(['# ' + line.strip() for line in wlines])
-
+            for l in self._get_wrapped_lines(line):
+                line_stripped = l.strip()
+                if line_stripped:
+                    result += '# ' + line_stripped + '\n'
+                else:
+                    result += '\n'
         return result
 
     def _get_arguments_text(self, arguments, force_empty=False):
@@ -59,36 +65,35 @@ class GDScriptWriter(Writer):
             self._write_description(file, klass)
             self._write_class_def(file, klass)
             self._write_constants(file, klass)
-            # self._write_newline(file)
-            # self._write_signals(file, klass)
-            # self._write_newline(file)
-            # self._write_members(file, klass)
-            # self._write_newline(file)
-            # self._write_methods(file, klass)
+            self._write_signals(file, klass)
+            self._write_members(file, klass)
+            self._write_methods(file, klass)
 
     def _write_brief_description(self, file, klass):
         if not klass.brief_description.strip():
             return
-        wlines = self._get_wrapped_lines(klass.brief_description.strip())
+
         raw_text = ''
-        raw_text += '\n'.join(['# ' + line.strip() for line in wlines])
-        if raw_text.strip():
-            raw_text = '#\n' + raw_text + '\n'
-            file.write(raw_text)
+        description_text = self._get_description_text(klass.brief_description)
+        if description_text.strip():
+            raw_text += '#\n' + description_text
+        file.write(raw_text)
 
     def _write_description(self, file, klass):
-        result = ''
+        if not klass.description.strip():
+            return
+
         raw_text = ''
-        for line in klass.description.split('\n'):
-            wlines = self._get_wrapped_lines(line)
-            raw_text += '\n'.join(['# ' + line.strip() for line in wlines])
-        if raw_text.strip():
-            result += '#\n' + raw_text + '\n'
-            result += '#\n'
-        file.write(result)
+        description_text = self._get_description_text(klass.description)
+        if description_text.strip():
+            raw_text += '#\n' + description_text
+        file.write(raw_text)
 
     def _write_class_def(self, file, klass):
-        raw_line = 'class {}'.format(klass.name)
+        raw_line = ''
+        if klass.brief_description.strip() or klass.description.strip():
+            raw_line += '#\n'
+        raw_line += 'class {}'.format(klass.name)
         if klass.inherits:
             raw_line += ' extends {}'.format(klass.inherits)
         raw_line += ':\n'
@@ -106,7 +111,7 @@ class GDScriptWriter(Writer):
         if constant.description:
             description_text = self._get_description_text(constant.description)
             if description_text.strip():
-                raw_text += '#\n' + description_text + '\n'
+                raw_text += '#\n' + description_text
                 raw_text += '#\n'
         raw_text += 'const ' + constant.name
         raw_text += ' = ' + constant.value if constant.value else ''
@@ -122,13 +127,17 @@ class GDScriptWriter(Writer):
             self._add_empty_line = True
 
     def _write_signal(self, file, signal, first):
-        text = ''
+        raw_text = '\n'
         if signal.description:
-            text += self._get_description_text(signal.description, not first)
-        text += 'signal ' + signal.name
-        text += self._get_arguments_text(signal.arguments)
-        text += '\n'
-        file.write(text)
+            description_text = self._get_description_text(signal.description)
+            if description_text.strip():
+                raw_text += '#\n' + description_text
+                raw_text += '#\n'
+        raw_text += 'signal ' + signal.name
+        indented_text = ''
+        for line in raw_text.split('\n'):
+            indented_text += self._make_line(line)
+        file.write(indented_text)
 
     def _write_members(self, file, klass):
         for idx, member in enumerate(klass.members):
@@ -137,12 +146,17 @@ class GDScriptWriter(Writer):
             self._add_empty_line = True
 
     def _write_member(self, file, member, first):
-        text = ''
+        raw_text = '\n'
         if member.description:
-            text += self._get_description_text(member.description, not first)
-        text += 'var {} # type: {}'.format(member.name, member.type)
-        text += '\n'
-        file.write(text)
+            description_text = self._get_description_text(member.description)
+            if description_text.strip():
+                raw_text += '#\n' + description_text
+                raw_text += '#\n'
+        raw_text += 'var {} # type: {}'.format(member.name, member.type)
+        indented_text = ''
+        for line in raw_text.split('\n'):
+            indented_text += self._make_line(line)
+        file.write(indented_text)
 
     def _write_methods(self, file, klass):
         for idx, method in enumerate(klass.methods):
@@ -151,19 +165,27 @@ class GDScriptWriter(Writer):
             self._add_empty_line = True
 
     def _write_method(self, file, method, first):
-        text = ''
+        raw_text = '\n'
         if method.description:
-            text += self._get_description_text(method.description, not first)
-        text += '# {}\n'.format(method.get_doc_link())
-        text += '#\n'
-        text += 'func ' + method.name
-        text += self._get_arguments_text(method.arguments, True)
-        text += ':'
+            description_text = self._get_description_text(method.description)
+            if description_text.strip():
+                raw_text += '#\n' + description_text
+                raw_text += '#\n'
+            else:
+                raw_text += '#\n'
+        raw_text += '# {}\n'.format(method.get_doc_link())
+        raw_text += '#\n'
+        raw_text += 'func ' + method.name
+        raw_text += self._get_arguments_text(method.arguments, True)
+        raw_text += ':'
         if method.return_type:
-            text += ' # returns: {}'.format(method.return_type)
-        text += '\n'
-        text += '    pass\n'
-        file.write(text)
+            raw_text += ' # returns: {}'.format(method.return_type)
+        raw_text += '\n'
+        raw_text += self._indent_line('pass')
+        indented_text = ''
+        for line in raw_text.split('\n'):
+            indented_text += self._make_line(line)
+        file.write(indented_text)
 
     def write(self, klass):
         self._write_class(klass)
